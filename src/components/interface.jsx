36c3e246e70d2ec869683a584cb6d4e9e20ffe1f
jj.attaq <jj.attaq@tutanota.com>
2023-02-26 22:53:25 +0100

    text updates in line with currentLocation, but currentText unused

--- src/components/interface.jsx
+++ src/components/interface.jsx
@@ -7,10 +7,10 @@ const log = console.log;
 export default function GameWindow() {
     const [currentLocation, setLocation] = useState(world[1][1]);
     const [currentText, setText] = useState(currentLocation.description);
-//    log(currentText);
+    // https://stackoverflow.com/questions/61569172/associate-a-state-with-another-state-in-usestate-hook
     return (
         <div className="game-window">
-            <Narrative text={currentText}/>
+            <Narrative text={currentLocation.description}/>
             <div className="window dashboard">
                 <Navigation setText={setText} currentLocation={currentLocation} setLocation={setLocation} />
                 <Interaction setText={setText}/>
@@ -36,30 +36,32 @@ function Navigation({setText, currentLocation, setLocation}) {
     const walkWest = "west";
     const walkEast = "east";
     const walkSouth = "south";
+    function handleMovement(direction) {
+        const newLoc = move(currentLocation, direction);
+        setLocation(newLoc);
+        setText(currentLocation.description);
+    }
     function handleKeyDown(e) {
         if (e.key === 'ArrowUp') {
             log('key pressed: ' + e.key);
+            handleMovement(walkNorth);
             // https://stackoverflow.com/questions/24943200/javascript-2d-array-indexof
-            setLocation(move(currentLocation, walkNorth))
-            setText(currentLocation.description)
         }
         if (e.key === 'ArrowLeft') {
             log('key pressed: ' + e.key);
-            setLocation(move(currentLocation, walkWest))
-            setText(currentLocation.description)
+            handleMovement(walkWest);
         }
         if (e.key === 'ArrowRight') {
             log('key pressed: ' + e.key);
-            setLocation(move(currentLocation, walkEast))
-            setText(currentLocation.description)
+            handleMovement(walkEast);
         }
         if (e.key === 'ArrowDown') {
             log('key pressed: ' + e.key);
-            setLocation(move(currentLocation, walkSouth))
-            setText(currentLocation.description)
+            handleMovement(walkSouth);
         }
     }
     useEffect(() => {
+        log(currentLocation);
         window.addEventListener('keydown', handleKeyDown)
         return () => { // this return is to clear the eventListener on mount
             log('listener removed')
@@ -70,13 +72,13 @@ function Navigation({setText, currentLocation, setLocation}) {
         <>
         <div className="navigation" >
             <div></div>
-            <Button value={"N"} type={"btn nav-btn"} onBtnClick={() => setLocation(move(currentLocation, walkNorth))}/>
+            <Button value={"N"} type={"btn nav-btn"} onBtnClick={() => handleMovement(walkNorth) }/>
             <div></div>
-            <Button value={"W"} type={"btn nav-btn"} onBtnClick={() => setText(walkWest)} />
+            <Button value={"W"} type={"btn nav-btn"} onBtnClick={() => handleMovement(walkWest) } />
             <div></div>
-            <Button value={"E"} type={"btn nav-btn"} onBtnClick={() => setText(walkEast)} />
+            <Button value={"E"} type={"btn nav-btn"} onBtnClick={() => handleMovement(walkEast)} />
             <div></div>
-            <Button value={"S"} type={"btn nav-btn"} onBtnClick={() => setText(walkSouth)} />
+            <Button value={"S"} type={"btn nav-btn"} onBtnClick={() => handleMovement(walkSouth) } />
             <div></div>
         </div>
         </>
